183db3140e1359651f2d02f5756908d18767c6ec
Athar <athar.imam81@gmail.com>
2023-12-25 15:45:01 +0530

    improve db connection

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require("dotenv").config({ path: "./env" });
+// improve version of upper code
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-/*
-// Approach one  o connect to db
+dotenv.config({
+  path: "./env",
+});
 
-import { Express } from "express";
-const app = express()(
-  // IIFe function
-  async () => {
-    try {
-      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-      app.on("error", (error) => {
-        console.log("err ", error);
-        throw error;
-      });
-
-      app.listen(process.env.PORT, () => {
-        console.log(`app is listening on port ${process.env.PORT}`);
-      });
-    } catch (error) {
-      console.log("ERROR ", error);
-      throw err;
-    }
-  }
-)();
-
-*/
+connectDB();
